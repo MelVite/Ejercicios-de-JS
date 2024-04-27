d3a809bebb2456d3522eecd391688a05031f894b
MelVite <melanievite38@gmail.com>
2024-04-26 18:17:34 -0600

    Actualización ejercicio 6

--- dominancia_positiva.js
+++ dominancia_positiva.js
@@ -5,3 +5,29 @@
  * positiveDom([-1, -3, -5, 4, 6767]) debería devolver false.
  */
 
+//Definimos la constante
+const arreglo = [-1, -3, -5, 4, 6767];
+
+function positiveDom(arr) {
+    // Contadores para llevar el seguimiento de los números positivos y negativos
+    let positive = 0;
+    let negative = 0;
+
+    // Iteración sobre el array y contar los números positivos y negativos
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            positive++;
+        } else if (arr[i] < 0) {
+            negative++;
+        }
+    }
+
+    // Condicional para que los elementos sean postivos 
+    if (positive > negative) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(positiveDom(arreglo))
